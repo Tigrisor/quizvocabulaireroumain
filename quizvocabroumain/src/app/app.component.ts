8251ded8649f13caebb222afa66f953c4111b21c
jdemay <jdemay@regionsjob.com>
2018-08-10 18:34:49 +0200

    [Feat] Ajout service + algo de base

--- quizvocabroumain/src/app/app.component.ts
+++ quizvocabroumain/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizvocabroumainService } from './services/quizvocabroumain.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'awsome app';
+export class AppComponent implements OnInit {
+  
+  listTest : any[];
+
+  currentQuestion : string;
+  currentAnswer : string;
+  currentIndex : number;
+  previousQuestion : string;
+  previousAnswer : string;
+  previousAnswerRightAnswer : string;
+
+  constructor(private quizVocabRoumainService: QuizvocabroumainService) {
+  }
+
+  ngOnInit()
+  {    
+    // c'est ici que l'on set la liste du vocabulaire local
+    this.listTest = this.quizVocabRoumainService.getListe();
+    this.currentIndex = 0;
+    this.currentQuestion = this.listTest[this.currentIndex].fr;
+  }
+
+  submitResponse() {
+    // on rempli les champs previous
+    this.previousAnswer=this.currentAnswer;
+    this.previousQuestion=this.currentQuestion;
+    this.previousAnswerRightAnswer=this.listTest[this.currentIndex].ro;
+    
+    // on passe a la question suivante
+    this.currentIndex++;
+    this.currentAnswer = '';
+    this.currentQuestion = this.listTest[this.currentIndex].fr;
+  }
 }
---
+++ quizvocabroumain/src/app/services/quizvocabroumain.service.ts
@@ -0,0 +1,33 @@
+export class QuizvocabroumainService {
+
+    getListe() {
+        return [
+            { ro: "cum", fr: "comment" },
+            { ro: "a saluta", fr: "saluer" },
+            { ro: "bun", fr: "bon" },
+            { ro: "dimineață", fr: "matin" },
+            { ro: "zi", fr: "jour" },
+            { ro: "seară", fr: "soir" },
+            { ro: "noapte", fr: "nuit" },
+            { ro: "la revedere!", fr: "au revoir!" },
+            { ro: "pe curînd!", fr: "à bientôt!" },
+            { ro: "pe mîine!", fr: "à demain!" },
+            { ro: "vă rog!", fr: "s'il vous plaît!" },
+            { ro: "poftiți!", fr: "voici!" },
+            { ro: "mulțumesc!", fr: "merci!" },
+            { ro: "cu plăcere!", fr: "avec plaisir, volontiers!" },
+            { ro: "la culcare", fr: "au coucher" },
+            { ro: "la plecare", fr: "au départ" },
+            { ro: "a citi", fr: "lire" },
+            { ro: "a spune", fr: "dire" },
+            { ro: "a cere", fr: "demander" },
+            { ro: "ceva", fr: "quelque chose" },
+            { ro: "a mulțumi", fr: "remercier" },
+            { ro: "el", fr: "il,lui" },
+            { ro: "bilet", fr: "billet" },
+            { ro: "pix", fr: "bic" },
+            { ro: "formular", fr: "formulaire" },
+            { ro: "creion", fr: "crayon" }
+        ]
+    }
+}
